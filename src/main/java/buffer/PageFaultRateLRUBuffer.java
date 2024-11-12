cefff9e1fa3cc75185ecfe7bfd48f8d45413b3b6
SenanM1 <senan.mardanli@gmail.com>
2024-11-12 20:22:52 +0100

    Task 1.b Done

--- src/main/java/buffer/PageFaultRateLRUBuffer.java
+++ src/main/java/buffer/PageFaultRateLRUBuffer.java
@@ -10,12 +10,21 @@ public class PageFaultRateLRUBuffer extends LRUBuffer {
 
     public double getFSR() {
         // TODO
-        return 0.0;
+        if (sCount == 0) return 0.0;
+        return (double) fsCount / sCount;
     }
 
     @Override
     protected Buffer.Slot fix(char c) throws IllegalStateException {
         // TODO
-        return null;
+        sCount++; // Increment total page access count
+
+        // Check if page is in the buffer
+        Buffer.Slot slot = lookUp(c);
+        if (slot == null) {
+            fsCount++;
+        }
+        
+        return super.fix(c);
     }
 }
