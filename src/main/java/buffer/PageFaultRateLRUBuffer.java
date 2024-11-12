6fa238df604f14fd9c30a1c2e886e54cf2f131be
SenanM1 <senan.mardanli@gmail.com>
2024-11-12 21:31:30 +0100

    Task 1.c Done

--- src/main/java/buffer/PageFaultRateLRUBuffer.java
+++ src/main/java/buffer/PageFaultRateLRUBuffer.java
@@ -24,7 +24,7 @@ public class PageFaultRateLRUBuffer extends LRUBuffer {
         if (slot == null) {
             fsCount++;
         }
-        
+
         return super.fix(c);
     }
 }
--- src/main/java/buffer/SimpleTwoQueueBuffer.java
+++ src/main/java/buffer/SimpleTwoQueueBuffer.java
@@ -11,16 +11,47 @@ public class SimpleTwoQueueBuffer extends PageFaultRateBuffer {
     public SimpleTwoQueueBuffer(int capacity) {
         super(capacity);
         // TODO
+        this.kin = capacity / 4; // 25% of total capacity
     }
 
     @Override
     protected Buffer.Slot fix(char c) throws IllegalStateException {
         // TODO
-        return null;
+        Slot slot = super.fix(c);
+
+        if(slot != null){
+            // If page is already in "am", move to the front
+            if(am.contains(slot)){
+                am.remove(slot);
+                am.addFirst(slot);
+            }
+            else if (a1.contains(slot)){
+                a1.remove(slot);
+                am.addFirst(slot);
+            }
+        } else {
+            // If super.fix(c) returns null, this is a page fault and 'c' was not in the buffer
+            slot = lookUp(c);
+
+            // Adding new page to "a1"
+            a1.addFirst(slot);
+
+            // If 'a1' exceeds its capacity 'kin', remove the oldest page
+            if(a1.size() > kin){
+                a1.removeLast();
+            }
+        }
+
+        return slot;
     }
 
     protected Slot victim() {
         // TODO
-        return null;
+        if (!a1.isEmpty()) {
+            return a1.removeLast(); // FIFO eviction from `a1`
+        } else if (!am.isEmpty()) {
+            return am.removeLast(); // LRU eviction from `am`
+        }
+        return null; // No victim if both queues are empty
     }
 }
